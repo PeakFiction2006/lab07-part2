0d68c0b4f308c4dd556f49ca07c37acf3ef51e48
PeakFiction2006 <105933774@student.swin.edu.au>
2025-05-01 12:07:16 +1000

    new

---
+++ process.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Booking Confirmation</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Rohirrim Booking Form" >
+	<meta name="keywords"    content="MiddleEarth, Tours, Rohan" >
+	<meta name="author"      content="Grima Wormtongue" />
+    <!-- Place the general style sheet before specific CSS so the specific overides the general formatting-->
+	<link rel="stylesheet" type="text/css" href="style/style.css" >
+	<link rel="stylesheet" type="text/css" href="style/register.css" >	
+</head>
+
+<body>
+  <h1>Rohirrim Tour Booking Confirmation</h1>
+</body>
+</html>
